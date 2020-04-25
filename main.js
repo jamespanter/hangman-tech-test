29a8080626315c770073ea5bd54352e2ec7fe581
jamespanter <james.panter@blueyonder.co.uk>
2020-04-25 15:34:41 +0100

    Added comments

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ let correctLettersArray,
   revealedLettersArray,
   wrongLettersArray = [];
 
+// Empty letter arrays and setup arrays for new word
 const resetGame = () => {
   wrongLettersArray = [];
   revealedLettersArray = [];
@@ -15,6 +16,7 @@ const resetGame = () => {
   updateHTML();
 };
 
+// Update HTML with current state of arrays
 const updateHTML = () => {
   wordContainer.innerHTML = revealedLettersArray
     .map((letter) => `<span>${letter} </span>`)
@@ -26,6 +28,8 @@ const updateHTML = () => {
   checkLose() ? (wrongLetterContainer.innerHTML = "Game over") : null;
 };
 
+// Check letter from the keydown event listener
+// Initialise action based on right or wrong letter
 const checkLetter = (letter) => {
   correctLettersArray.includes(letter)
     ? addCorrectLetter(letter)
@@ -33,6 +37,7 @@ const checkLetter = (letter) => {
   updateHTML();
 };
 
+// Change any correct letters from '_' to actual letter in RevealedLettersArray
 const addCorrectLetter = (letter) => {
   let indexesOfCorrectLetter = [];
   correctLettersArray.forEach((char, index) =>
@@ -44,6 +49,7 @@ const addCorrectLetter = (letter) => {
   }
 };
 
+// Add incorrect letter to wrongLettersArray if it is not already present
 const addWrongLetter = (letter) =>
   wrongLettersArray.includes(letter) ? null : wrongLettersArray.push(letter);
 
@@ -51,6 +57,7 @@ const checkLose = () => wrongLettersArray.length >= 6;
 
 const checkWin = () => !revealedLettersArray.includes("_");
 
+// Check keydown is a letter and that the game is not already won/lost and call checkLetter function
 window.addEventListener("keydown", (e) => {
   e.keyCode >= 65 && e.keyCode <= 90 && !checkWin() && !checkLose()
     ? checkLetter(e.key.toUpperCase())
